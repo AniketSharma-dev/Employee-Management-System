ead6c39560a40b06b6fd2d74e5dbfb20ec8aa1d6
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-22 22:17:36 +0530

    Login Section

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(
+      "User Email : " + email + "\nUser Password : " + password + "\nSubmited"
+    );
+    setEmail("");
+    setPassword("");
+  };
 
   return (
     <div className=" w-full h-screen flex items-center justify-center ">
       <div className="border-2 rounded-2xl px-32 py-28 border-emerald-700">
-        <form className="w-full flex flex-col items-center justify-center gap-7">
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="w-full flex flex-col items-center justify-center gap-7"
+        >
           <div className="flex flex-col gap-3">
             <input
-              className="border-2 border-blue-600 rounded-full outline-none bg-transparent px-5 py-3  placeholder:text-gray-400"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               type="email"
               placeholder="Enter Your Email"
+              className="border-2 border-blue-600 rounded-full outline-none bg-transparent px-5 py-3  placeholder:text-gray-400"
               required
             />
             <input
-              className="border-2 border-blue-600 rounded-full outline-none bg-transparent px-5 py-3  placeholder:text-gray-400"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
               type="password"
               placeholder="Enter Your Password"
+              className="border-2 border-blue-600 rounded-full outline-none bg-transparent px-5 py-3  placeholder:text-gray-400"
               required
             />
           </div>
