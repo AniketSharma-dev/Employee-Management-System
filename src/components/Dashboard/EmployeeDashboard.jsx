d24db41ced0e0594aaa1ae140c88818e12dd0f1f
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-23 23:41:02 +0500

    Path Updated

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Header from "./DashBNavbar/header";
-import TaskNumber from "./taskNumber/TaskNumber";
 import TaskList from "../TaskList/TaskList";
+import Header from "./DashboardCompo/DashBNavbar/Header";
+import TaskNumber from "./DashboardCompo/taskNumber/TaskNumber";
 
 const EmployeeDashboard = () => {
     return (
         <div className="bg-[#1C1C1C] w-full  px-20  h-screen">
             <Header />
-            <TaskNumber />
-            <TaskList/>
+            <TaskNumber/>
+            <TaskList />
         </div>
     );
 };
